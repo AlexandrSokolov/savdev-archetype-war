c4f09652d869a723acacca59d2355f903c0d0078
alexandr <alexandrsokolov@users.noreply.github.com>
2020-05-28 15:18:51 +0200

    number attributes support

--- multi_module_war_template/domain_api/src/main/java/com/savdev/some/project/api/ExampleApi.java
+++ multi_module_war_template/domain_api/src/main/java/com/savdev/some/project/api/ExampleApi.java
@@ -9,4 +9,24 @@ public interface ExampleApi {
   String getName();
 
   void setName(String name);
+
+  short getSmallestInt();
+
+  void setSmallestInt(short smallestInt);
+
+  short getSmallInt();
+
+  void setSmallInt(short smallInt);
+
+  int getNumber1();
+
+  void setNumber1(int number1);
+
+  int getNumber2();
+
+  void setNumber2(int number2);
+
+  long getBigNumber();
+
+  void setBigNumber(long bigNumber);
 }
--- multi_module_war_template/sql_datasource/src/main/java/com/savdev/some/project/datasource/entities/EntityExample.java
+++ multi_module_war_template/sql_datasource/src/main/java/com/savdev/some/project/datasource/entities/EntityExample.java
@@ -18,6 +18,22 @@ public class EntityExample implements ExampleApi {
 
   private String name;
 
+  //liquibase type: tinyint
+  private short smallestInt;
+
+  //liquibase type: smallint
+  private short smallInt;
+
+  //liquibase type: number
+  private int number1;
+
+  //liquibase type: int
+  private int number2;
+
+  //liquibase type: bigint
+  private long bigNumber;
+
+
   @Override
   public long getId() {
     return id;
@@ -36,5 +52,55 @@ public class EntityExample implements ExampleApi {
   @Override
   public void setName(String name) {
     this.name = name;
+  }
+
+  @Override
+  public short getSmallestInt() {
+    return smallestInt;
+  }
+
+  @Override
+  public void setSmallestInt(short smallestInt) {
+    this.smallestInt = smallestInt;
+  }
+
+  @Override
+  public short getSmallInt() {
+    return smallInt;
+  }
+
+  @Override
+  public void setSmallInt(short smallInt) {
+    this.smallInt = smallInt;
+  }
+
+  @Override
+  public int getNumber1() {
+    return number1;
+  }
+
+  @Override
+  public void setNumber1(int number1) {
+    this.number1 = number1;
+  }
+
+  @Override
+  public int getNumber2() {
+    return number2;
+  }
+
+  @Override
+  public void setNumber2(int number2) {
+    this.number2 = number2;
+  }
+
+  @Override
+  public long getBigNumber() {
+    return bigNumber;
+  }
+
+  @Override
+  public void setBigNumber(long bigNumber) {
+    this.bigNumber = bigNumber;
   }
 }
